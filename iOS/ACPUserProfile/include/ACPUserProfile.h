cc7369ffcf23a81d435ce7682729feb7fc2b8bcb
steve benedick <sbenedic@adobe.com>
2019-01-22 17:03:34 -0700

    -user profile files for manual install

---
+++ iOS/ACPUserProfile/include/ACPUserProfile.h
@@ -0,0 +1,56 @@
+//
+//  ACPUserProfile.h
+//  Adobe Digital Marketing Suite -- iOS Application Measurement Library
+//
+//  Copyright 1996-2019. Adobe, Inc. All Rights Reserved
+//
+//  UserProfile Version: 2.0.0
+
+#import <Foundation/Foundation.h>
+
+@interface ACPUserProfile : NSObject {}
+
+/**
+ * @brief Returns the current version of the ACPUserProfile Extension.
+ */
++ (nonnull NSString*) extensionVersion;
+
+/**
+ * @brief Registers the UserProfile extension
+ *
+ * This method should be called before any other User Profile API is called
+ */
++ (void) registerExtension;
+
+/**
+ * UserProfile API to remove the give attribute name
+ *
+ * If the attribute does not exist, this API has no effects
+ * If the attribute exists, then the User Attribute will be removed
+ *
+ * @param attributeName Attribute key which has to be removed.
+ */
++ (void) removeUserAttribute: (nonnull NSString*) attributeName;
+
+/**
+ * UserProfile API to set user profile attributes keys and values.
+ *
+ * If the attribute does not exist, it will be created. If the attribute
+ * already exists, then the value will be updated. An empty attribute value will remove the attribute.
+ *
+ * @param attributeName  Attribute key.
+ * @param attributeValue Attribute value corresponding to the key.
+ */
++ (void) updateUserAttribute: (nonnull NSString*) attributeName withValue: (nullable NSString*) attributeValue;
+
+/**
+ * UserProfile API to set user profile attributes keys and values.
+ *
+ * If the attribute does not exist, it will be created. If the attribute
+ * already exists, then the value will be updated. A empty attribute value will remove the attribute.
+ *
+ * @param attributeMap of profile attributes key-value pairs to be set.
+ */
++ (void) updateUserAttributes: (nonnull NSDictionary*) attributeMap;
+
+@end
