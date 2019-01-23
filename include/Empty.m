c4fd4b13adea07aea1c6f5180767a49684191286
Kevin Lind <lind@adobe.com>
2019-01-23 12:46:31 -0800

    add copyright to Empty.m

--- include/Empty.m
+++ include/Empty.m
@@ -1,3 +1,9 @@
-  #import "ACPCore.h"
-  @implementation ACPCore (ForceLoad) @end
+//
+// Empty.m
+// Adobe Experience Platform -- iOS Library
+// 
+// Copyright 1996-2019. Adobe. All Rights Reserved
+
+ #import "ACPCore.h"
+ @implementation ACPCore (ForceLoad) @end
 
