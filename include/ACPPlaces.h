44526dafc008ca130c41c1bd907d6b41efae3fd8
steve benedick <sbenedic@adobe.com>
2020-02-20 15:32:08 -0700

    ios places 1.3.1

--- include/ACPPlaces.h
+++ include/ACPPlaces.h
@@ -4,7 +4,7 @@
 //
 //  Copyright 1996-2018. Adobe. All Rights Reserved
 //
-//  Places Version: 1.3.0
+//  Places Version: 1.3.1
 
 #import <CoreLocation/CoreLocation.h>
 #import <Foundation/Foundation.h>
