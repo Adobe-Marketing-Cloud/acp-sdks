b888674e4c1aa6975a3dcebc72e9d15f58a17be2
amsdkci <amsdkci@amsdkcis-mbp.corp.adobe.com>
2020-03-02 11:53:27 -0700

    Release ACPAnalytics v2.2.3 for manual install. 🚀

--- iOS/ACPAnalytics/include/ACPAnalytics.h
+++ iOS/ACPAnalytics/include/ACPAnalytics.h
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 
-Analytics Version: 2.2.2
+Analytics Version: 2.2.3
 */
 
 #import <Foundation/Foundation.h>
