37bf676c56431ee51b80fd5488cfd148b702fb3a
amsdkci <amsdkci@amsdkcis-mbp.corp.adobe.com>
2020-06-16 15:52:04 -0600

    Release ACPLifecycle v2.1.0 for pod install. 🚀

--- include/ACPLifecycle.h
+++ include/ACPLifecycle.h
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 
-Lifecycle Version: 2.0.4
+Lifecycle Version: 2.1.0
 */
 
 #import <Foundation/Foundation.h>
