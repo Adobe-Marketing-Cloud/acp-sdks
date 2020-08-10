9236bef1a3702c6803332fc439d84ae992a83805
amsdkci <amsdkci@amsdkcis-mbp.corp.adobe.com>
2020-08-10 14:52:53 -0600

    Release ACPLifecycle v2.1.1 for manual install. 🚀

--- tvOS/ACPCore/include/ACPLifecycle.h
+++ tvOS/ACPCore/include/ACPLifecycle.h
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 
-Lifecycle Version: 2.1.0
+Lifecycle Version: 2.1.1
 */
 
 #import <Foundation/Foundation.h>
