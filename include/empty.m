973bd5fba4f33ea8c083ba4b9b218790d4bcd7e5
Ryan Morales <rymorale@adobe.com>
2020-12-18 10:10:04 -0800

    remove old files and update podspec

--- include/empty.m
+++
@@ -1,7 +0,0 @@
-#import "ACPTarget.h"
-#import "ACPTargetOrder.h"
-#import "ACPTargetParameters.h"
-#import "ACPTargetPrefetchObject.h"
-#import "ACPTargetProduct.h"
-#import "ACPTargetRequestObject.h"
-@implementation ACPTarget (ForceLoad) @end
