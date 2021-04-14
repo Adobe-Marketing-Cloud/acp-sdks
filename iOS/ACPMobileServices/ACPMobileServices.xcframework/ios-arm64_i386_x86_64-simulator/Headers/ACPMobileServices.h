79c1d4e8226d17c992177ac96109fdf9027803c7
amsdkci <amsdkci@amsdkci03.corp.adobe.com>
2021-04-14 10:17:38 -0600

    Release ACPMobileServices v1.1.1 for manual install. 🚀

--- iOS/ACPMobileServices/ACPMobileServices.xcframework/ios-arm64_i386_x86_64-simulator/Headers/ACPMobileServices.h
+++ iOS/ACPMobileServices/ACPMobileServices.xcframework/ios-arm64_i386_x86_64-simulator/Headers/ACPMobileServices.h
@@ -3,7 +3,7 @@
 //  Adobe Experience Platform -- iOS Framework
 //
 //  Copyright 1996-2019. Adobe. All Rights Reserved
-//  Mobile Services Version: 1.1.0
+//  Mobile Services Version: 1.1.1
 
 
 #import <Foundation/Foundation.h>
