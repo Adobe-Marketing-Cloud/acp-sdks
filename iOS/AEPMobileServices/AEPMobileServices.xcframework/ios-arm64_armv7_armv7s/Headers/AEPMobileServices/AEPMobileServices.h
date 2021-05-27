8e19506a9b0d44a97f3bd21079a109276bf95ac5
jgeng <jgeng@adobe.com>
2021-05-27 16:17:51 -0600

    AEP Mobile services iOS manual install libs

---
+++ iOS/AEPMobileServices/AEPMobileServices.xcframework/ios-arm64_armv7_armv7s/Headers/AEPMobileServices/AEPMobileServices.h
@@ -0,0 +1,31 @@
+//
+//  AEPMobileServices.h
+//  Adobe Experience Platform -- iOS Framework
+//
+//  Copyright 1996-2021. Adobe. All Rights Reserved
+//  Mobile Services Version: 3.0.0
+
+
+#import <Foundation/Foundation.h>
+@import AEPCore;
+
+@interface AEPMobileServices : NSObject <AEPExtension>
+
+/**
+ * @brief Returns the current version of the AEPMobileServices Extension.
+ */
++ (nonnull NSString*) extensionVersion;
+
+/**
+ * @brief Marshalls the provided deeplink and generates an event containing its data.
+ *
+ * This method should be called within the application:openURL:options: app delegate method.
+ *
+ * If Analytics is configured in the SDK, calling this method with a valid URL will result in an
+ * Analytics request being sent.
+ *
+ * @param deeplink the NSURL that the application is processing
+ */
++ (void) trackAdobeDeepLink: (NSURL* _Nonnull) deeplink;
+
+@end
